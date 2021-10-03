bde0392a96ef77858c3540d0db6626b5597de557
Sebastian Cardona <sebasdeveloper89@gmail.com>
2021-10-03 10:56:54 -0500

    Se crea la aplicacion de consola

---
+++ MiPrimeraApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPrimeraApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mi Primera Aplicacion - Sebastian Cardona");
+            Console.ReadLine();
+        }
+    }
+}
